919f495a8fc2156c05f9c085a742f5c72c5aef46
Pipemma <dremmaokoh@gmail.com>
2023-05-19 22:56:06 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -6,16 +6,6 @@ const connectDB = async () => {
    .catch((err) => console.log (err))
 };
 
-// const connectDB = async () => {
-//     try {
-//         mongoose.set('strictQuery', false)
-//         mongoose.connect(process.env.MONGODB_URL) 
-//         console.log('Mongo connected')
-//     } catch(error) {
-//         console.log(error)
-//         process.exit()
-//     }
-// }
 
 module.exports = connectDB;
 
